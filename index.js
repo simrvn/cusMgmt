412d25130a3d1b6eb17b2a040ce4955ff590618a
Simranjit Kaur <simranjit.kaur@thoughtworks.com>
2022-12-27 22:10:05 +0800

    modified UI for search and delete, refactored delete user logic

--- index.js
+++ index.js
@@ -1,25 +1,61 @@
 "use strict";
-import { createCustomer, deleteCustomer, userStorage, searchCustomer} from "./src/repository/CustomerRepository.js";
+import {
+  createCustomer,
+  deleteCustomer,
+  userStorage,
+  searchCustomer,
+} from "./src/repository/CustomerRepository.js";
 
 document.getElementById("add-button").addEventListener("click", () => {
-  const firstName = document.getElementById("first-name").value;
-  const lastName = document.getElementById("last-name").value;
+  const firstName = document.getElementById("add-first-name").value;
+  const lastName = document.getElementById("add-last-name").value;
 
   createCustomer(firstName, lastName);
   console.log(userStorage);
-
 });
 
-document.getElementById("remove-button").addEventListener("click",() => {
-  const firstName = document.getElementById("remove-first-name").value;
-  const lastName = document.getElementById("remove-last-name").value;
+const deleteButton = document.getElementById("delete");
+if (deleteButton){
+  deleteButton.addEventListener("click", () => {
+    const firstName = document.getElementById("first-name").textContent;
+    const lastName = document.getElementById("last-name").textContent;
+    console.log(lastName);
+    deleteCustomer(firstName, lastName);
+    displayCustomers(userStorage);
+  });
+};
 
-  deleteCustomer(firstName, lastName);
-  console.log(userStorage);
 
-});
-
-document.getElementById("search-button").addEventListener('click', () => {
+document.getElementById("search-button").addEventListener("click", () => {
   const input = document.getElementById("search").value;
-    console.log(searchCustomer(input));
+
+  const filteredList = searchCustomer(input);
+
+  displayCustomers(filteredList);
+
 });
+
+
+const displayCustomers = (userList) =>{
+
+  document.querySelector(".table-header").style.display = "contents";
+
+  const userTable = document.querySelector(".user-table");
+  let dataHtml = '';
+
+  console.log(userList);
+
+  for (let i=0; i<userList.length; i++){
+    dataHtml += `
+      <tr>
+      <td id="first-name">${userList[i].firstName}</td>
+      <td id="last-name">${userList[i].lastName}</td>
+      <td>
+          <button id="delete" class="btn delete">Remove</button>
+      </td>
+      </tr>
+    `;
+  }
+  userTable.innerHTML = dataHtml;
+}
+
--- src/repository/CustomerRepository.js
+++ src/repository/CustomerRepository.js
@@ -13,12 +13,11 @@ const deleteCustomer = (firstName, lastName) => {
   if (userStorage.length === 0)
     console.log("There are no users in the database");
 
-  for (var i = 0; i < userStorage.length; i++) {
-    if (isNameEqual(userStorage[i], customer)) {
-      userStorage.splice(i, 1);
-      break;
-    }
-  }
+  const index = userStorage.findIndex(
+    (cust) => cust.firstName === firstName && cust.lastName === lastName
+  );
+
+  userStorage.splice(index, 1);
 };
 
 const searchCustomer = (input) => {
@@ -27,8 +26,9 @@ const searchCustomer = (input) => {
   );
 };
 
-const isNameEqual = (obj1, obj2) => {
-  return obj1.firstName === obj2.firstName && obj1.lastName === obj2.lastName;
+export {
+  createCustomer,
+  deleteCustomer,
+  userStorage,
+  searchCustomer,
 };
-
-export { createCustomer, deleteCustomer, isNameEqual, userStorage, searchCustomer };
